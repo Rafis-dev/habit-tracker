d84b0e52d9b69a6103de52218ffc18d49c2970ac
Rafis Shigapov <rafis1work@gmail.com>
2026-07-12 18:53:52 +0300

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import Header from './components/Header';
+
 export default function App() {
-  return <h1></h1>;
+  return (
+    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+      <Header />
+    </div>
+  );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+
+type ButtonProps = {
+  children: ReactNode;
+};
+
+export default function Button({ children }: ButtonProps) {
+  return (
+    <button
+      className="bg-violet-600 hover:bg-violet-500 hover:cursor-pointer transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed
+      disabled:hover:bg-violet-600
+    "
+    >
+      {children}
+    </button>
+  );
+}
